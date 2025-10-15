26e45c938a79111364eaf801ee84868cef535ada
SonarJayesh <jayeshsonar2824@gmail.com>
2025-10-15 22:31:43 +0530

    [ADDED: JAYESH This JDBC code Used For inserting the Records in the database using execute the query statement]

---
+++ DBConnection4.java
@@ -0,0 +1,38 @@
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+
+class DBConnection4{
+
+	public static void main(String[] args){
+		
+		try{
+		    //Step1 Register the Driver
+			Driver d = new oracle.jdbc.driver.OracleDriver();
+			DriverManager.registerDriver(d);
+			System.out.println("Driver Register Successfully!!");
+
+		    //Step2 Get the Connection
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521","system","Jayesh");
+			System.out.println("Connection Succsfully Done.. Connection Id:= "+con);
+
+		    //Step3 Created Statement Object 
+			Statement stmt = con.createStatement();
+			System.out.println("Statement Object Created...");
+			
+		    //Step4 Exicute the Query
+			//stmt.executeUpdate("create table student(id number(3),name varchar2(10))");
+			stmt.executeUpdate("insert into student values(1,'Jayesh')");
+			System.out.println("Query Exicuted..");
+			
+ 		    //Step5 Close Connection
+			con.close();		
+	
+		}
+		catch(SQLException ex){
+			System.out.println("Error in Connection!!");
+		}
+	}
+}
